3c6985a1819e7c3f6134a97d038c812a8e84d93a
Łukasz Kochajewski <lkochajewski@pgs-soft.com>
2022-01-17 15:46:42 +0100

    Update AppLink styles

--- src/components/atoms/AppLink.tsx
+++ src/components/atoms/AppLink.tsx
@@ -3,19 +3,15 @@ import React from 'react';
 import { styled } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 
-const StyledLink = styled(NavLink)<{ isActive: boolean }>`
+const StyledLink = styled(NavLink)`
   text-decoration: none;
-
-  &:focus,
-  &:hover,
-  &:visited,
-  &:link,
-  &:active {
-    text-decoration: none;
-  }
   color: white;
   font-weight: 600;
   margin: 0 10px;
+  &:focus,
+  &:active {
+    text-decoration: underline;
+  }
 `;
 
 export default function AppLink(props: {
@@ -23,21 +19,5 @@ export default function AppLink(props: {
   label: string;
   exact: boolean;
 }) {
-  const activeStyle = {
-    textDecoration: 'underline',
-  };
-  const inActiveStyle = {
-    textDecoration: 'none',
-  };
-
-  return (
-    <StyledLink
-      // eslint-disable-next-line no-undef
-      isActive={isActive}
-      style={({ isActive }) => (isActive ? activeStyle : inActiveStyle)}
-      {...props}
-    >
-      {props.label}
-    </StyledLink>
-  );
+  return <StyledLink {...props}>{props.label}</StyledLink>;
 }
