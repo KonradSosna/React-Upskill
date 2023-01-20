409939a5e6a6bc16ac336e780f9b46107def65f3
Konrad Sosna <konrad.sosna.95@gmail.com>
2023-01-20 14:26:08 +0100

    Init Repo

--- src/hooks/useInvoices.ts
+++ src/hooks/useInvoices.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Invoice } from '../intefaces/invoices';
 import ApiService from '../services/api';
--- src/hooks/useProgressInterceptor.ts
+++ src/hooks/useProgressInterceptor.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import ApiService from '../services/api';
 import { registerInterceptor } from '../services/http.interceptor';
